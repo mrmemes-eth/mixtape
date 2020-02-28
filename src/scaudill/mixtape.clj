4290d24578426e55e5293b76932ee98fc50d9360
Stephen Caudill <scaudill@gmail.com>
2020-02-28 15:34:32 -0700

    Exit when input validation occurs

--- src/scaudill/mixtape.clj
+++ src/scaudill/mixtape.clj
@@ -4,23 +4,27 @@
   (:gen-class))
 
 (def cli-options
-  [["-t" "--mixtape" "Path to your mixtape JSON file."
-    :default "mixtape.json"
+  [["-t" "--mixtape MIXTAPE.JSON" "Path to your mixtape JSON file."
+    :default "resources/mixtape.json"
     :validate [(complement empty?)
                "Please supply a path to a mixtape JSON file or accept the defaults."]]
-   ["-c" "--changes" "Path to your changes JSON file."
+   ["-c" "--changes CHANGES.JSON" "Path to your changes JSON file."
     :default "changes.json"
     :validate [(complement empty?)
                "Please supply a path to a changes JSON file or accept the defaults."]]
    ["-h" "--help"]])
 
+(defn exit
+  [status message]
+  (println message)
+  (System/exit status))
+
 (defn -main
   "Consume and modify playlist files"
   [& args]
-  (let [{:keys [options errrors summary errors]} (cli-tools/parse-opts args cli-options)]
+  (let [{:keys [options errrors summary errors] :as opts} (cli-tools/parse-opts args cli-options)]
     (cond
       (:help options) (println summary)
       errors (->> (str/join \newline errors)
                   (str "The following errors occurred:\n\n")
-                  println))
-    (println options)))
+                  (exit 1)))))
