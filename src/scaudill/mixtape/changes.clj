2d7f29b75150e021ede9131fd908225a9df17ce2
Stephen Caudill <scaudill@gmail.com>
2020-02-28 20:34:07 -0700

    Better names in the changes namespace

--- src/scaudill/mixtape/changes.clj
+++ src/scaudill/mixtape/changes.clj
@@ -1,15 +1,20 @@
 (ns scaudill.mixtape.changes
   (:require [scaudill.mixtape.manipulate :as manipulate]))
 
-(def command-matrix
+(def command-fns
+  "Maps commands from our JSON to functions that will apply the desired changes."
   {:add_playlist manipulate/append-playlist
    :add_song_to_playlist manipulate/add-song-to-playlist
    :remove_playlist manipulate/remove-playlist})
 
-(defn command-applicator
-  [acc command command-data]
-  (reduce (partial (command command-matrix)) acc command-data))
+(defn apply-individual-commands
+  "Given `data`, it applys the change specified by `change-command` to each set
+  of arguments in `payloads`."
+  [data change-command payloads]
+  (reduce (partial (change-command command-fns)) data payloads))
 
 (defn apply-changes
+  "Given the mixtape `data`, and the set of `changes`, return the `data` with
+   the `changes` applied."
   [data changes]
-  (reduce-kv command-applicator data changes))
+  (reduce-kv apply-individual-commands data changes))
